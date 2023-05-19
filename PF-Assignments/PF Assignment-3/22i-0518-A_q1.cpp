083c0f4d76a05fb7ce8fad85087827491599ece1
Maleek Hussain Ali <115790326+MaleekNoob@users.noreply.github.com>
2023-05-19 12:30:10 +0500

    Update 22i-0518-A_q1.cpp
    improved readability

--- PF-Assignments/PF Assignment-3/22i-0518-A_q1.cpp
+++ PF-Assignments/PF Assignment-3/22i-0518-A_q1.cpp
@@ -7,29 +7,31 @@
 using namespace std;
 int main()
 {   
-    float score,more=0,sum=0;
+    float score, more = 0, sum = 0;
     float mean;         
-    for(int i=1;i<=5;i++)               //loop runs 5 times
+    for(int i = 1; i <= 5; i++)               //loop runs 5 times
     {
-        cout<<"Enter score of subject "<<i<<" : ";
-        cin>>score;                                 // 5 scores are entered
-        if(score<=100 && score >=0)    //scores should be in valid range of (0-100)    
+        cout << "Enter score of subject " << i << " : ";
+        cin >> score;                                 // 5 scores are entered
+        if(score <= 100 && score >= 0)    //scores should be in valid range of (0-100)    
         {
-            if(score>=80)         //if score is more than 80 add one to counter
+            if(score >= 80)         //if score is more than 80 add one to counter
             {
-                more++;
+                ++more;
             }
-            sum=sum+score;         //sum up all the scores one by one
+            sum = sum + score;         //sum up all the scores one by one
         }
         else           //if out of range number is entered 
         {
-            cout<<"Invalid Input, Enter in valid range (0-100)"<<endl;
-            i--;            //dont count that iteration and redo the loop
+            cout << "Invalid Input, Enter in valid range (0-100)" << endl;
+            --i;            //dont count that iteration and redo the loop
             continue;
         }
     }
-    cout<<endl<<endl;
-    mean=sum/5;             //print the mean and num of scores more than 80
-    cout<<"Mean of scores : "<<mean<<endl;
-    cout<<"There are "<<more<<" scores which are more than 80"<<endl;
+    cout << endl << endl;
+    mean = sum/5;             //print the mean and num of scores more than 80
+    cout << "Mean of scores : " << mean << endl;
+    cout << "There are " << more << " scores which are more than 80" << endl;
+    
+    return 0;
 }
